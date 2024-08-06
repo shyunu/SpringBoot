82aea47db0671ec62a66e2f834e46b7df3155c4a
suhyun-kim9 <ksh020674@gmail.com>
2024-08-06 17:22:06 +0900

    BootBasic 수업(240806)

---
+++ BootBasic/src/main/java/com/simple/basic/command/TestVO.java
@@ -0,0 +1,15 @@
+package com.simple.basic.command;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor //모든 멤버변수를 받는 생성자
+@NoArgsConstructor //기본 생성자
+@Data //getter, setter 생성, toString 오버라이딩 자듕
+public class TestVO {
+
+    private String name;
+    private int age;
+    private String addr;
+}
